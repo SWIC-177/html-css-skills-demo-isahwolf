3838ebd3bbb6db71172dd77b627228c517f4c9df
isahwolf <isaiah.wolf@student.swic.edu>
2024-05-10 13:19:04 -0500

    feat: Created hideError and renderError to display and hide error messages

--- src/utils.js
+++ src/utils.js
@@ -24,3 +24,19 @@ validate(val) {
     },
 },
 ];
+
+export const hideError = (el) => {
+    el.parentNode.querySelector(".error").classList.remove("is-error");
+};
+
+export const renderError = (el, msg) => {
+    const errorEl = el.parentNode.querySelector(".error");
+
+    if (errorEl) errorEl.classList.add("is-error");
+    else {
+        const errorElement = document.createElement("p");
+        errorElement.className = "error is-error";
+        errorElement.textContent = msg;
+        el.parentNode.appendChild(errorElement);
+    }
+};
